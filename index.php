58e48a17dbb621a7cc45cf1fa09eedb56fb31096
fedepaloz <palozzifederico@gmail.com>
2022-08-29 17:41:46 +0200

    finished

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php 
 $citazione = "la logica è l'anatomia del pensiero";
-$array = explode(" ", $citazione);
-var_dump ($array)
-?>
+$lunghezza_citazione = strlen($citazione);
+$bad_word = $_GET['badword'];
+$censored_word = '***';
+$nuova_citazione = str_replace('logica','***', $citazione);
+$lunghezza_nuova_citazione = strlen($nuova_citazione);
+ ?>
 
+<h1>john locke disse: <?php echo ($nuova_citazione)?></h1>
+
+<h2>la citazione è lunga:<?php var_dump($lunghezza_citazione)?></h2>
+
+<h2>la nuova citazione è lunga: <?php var_dump($lunghezza_nuova_citazione)?></h2>
