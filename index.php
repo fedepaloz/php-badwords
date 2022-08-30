2e6d42139227ec2b7fbce6bd648302887e46de7f
fedepaloz <palozzifederico@gmail.com>
2022-08-30 23:00:03 +0200

    finished

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
-<?php 
-$citazione = "la logica è l'anatomia del pensiero";
-$lunghezza_citazione = strlen($citazione);
-$bad_word = $_GET['badword'];
-$censored_word = '***';
-$nuova_citazione = str_replace('logica','***', $citazione);
-$lunghezza_nuova_citazione = strlen($nuova_citazione);
- ?>
+<?php
+$paragrafo = ("amor, ch'al cor gentil ratto s'apprende,
+prese costui de la bella persona
+che mi fu tolta; e 'l modo ancor m'offende.
+amor, ch'a nullo amato amar perdona,
+mi prese del costui piacer sì forte,
+che, come vedi, ancor non m'abbandona.
+amor condusse noi ad una morte:
+Caina attende chi vita ci spense.");
 
-<h1>john locke disse: <?php echo ($nuova_citazione)?></h1>
+echo($paragrafo);
 
-<h2>la citazione è lunga:<?php var_dump($lunghezza_citazione)?></h2>
+$censoredWord  = ($_GET["badword"]);
 
-<h2>la nuova citazione è lunga: <?php var_dump($lunghezza_nuova_citazione)?></h2>
+$lunghezzaParagrafo = strlen($paragrafo);
+
+$paragrafoCensurato = str_replace($censoredWord,'***',$paragrafo);
+
+$lunghezzaParagrafoCensurato = strlen($paragrafoCensurato);
+
+echo $paragrafoCensurato
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo($paragrafo) ?></h1>
+    <h2>il paragrafo è lungo <?php echo ($lunghezzaParagrafo) ?> caratteri</h2>
+<h3>paragrafo censurato qui sotto</h3>
+<h1><?php echo($paragrafoCensurato) ?></h1>
+<h2>il paragrafo è lungo <?php echo ($lunghezzaParagrafoCensurato) ?> caratteri</h2>
+</body>
+</html>
